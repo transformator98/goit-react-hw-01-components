d99c8a2e65bcb6d307a5af4a6473c89991546a3b
transformator98 <vanushka16@gmail.com>
2020-11-25 01:58:47 +0200

    added component profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile';
+import profiles from './user.json';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        name={profiles.name}
+        tag={profiles.tag}
+        lacation={profiles.location}
+        avatar={profiles.avatar}
+        followers={profiles.stats.followers}
+        views={profiles.stats.views}
+        likes={profiles.stats.likes}
+      />
     </div>
   );
 }
-
-export default App;
