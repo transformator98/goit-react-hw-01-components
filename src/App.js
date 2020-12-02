b30d1b91ee764e906317a76a6a10d23f8ee56d8f
transformator98 <vanushka16@gmail.com>
2020-12-02 22:35:51 +0200

    added profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import ProfileList from './components/Profile/ProfileList';
 // import Section from './components/Statistics/Section';
-// import Statistic from './components/Statistics/Statistics';
+import Statistic from './components/Statistics/Statistics';
 import profiles from './user.json';
-// import statistics from './statistics.json';
+import statistics from './statistics.json';
 import s from './components/Profile/Profile.module.css';
 
 export default function App() {
   return (
     <div className={s.container}>
       <ProfileList profiles={profiles} />
+      <Statistic
+        key={statistics.id}
+        labels={statistics.label}
+        percentages={statistics.percentage}
+      />
     </div>
   );
 }
@@ -16,8 +21,8 @@ export default function App() {
 //   /* <Section>
 //         <Statistic
 //           key={statistics.id}
-//           label={statistics.label}
-//           percentage={statistics.percentage}
+//           labels={statistics.label}
+//           percentages={statistics.percentage}
 //         />
 //       </Section> */
 // }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,30 +1,23 @@
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
-export default function Statistics({ label, percentage }) {
+export default function Statistics({ labels, percentages }) {
   return (
     <ul className={s.stat}>
       <li className={s.item}>
-        <span className={s.label}>{label}</span>
-        <span className={s.percentage}>{percentage}%</span>
-      </li>
-      <li className={s.item}>
-        <span className={s.label}>{label}</span>
-        <span className={s.percentage}>{percentage}%</span>
-      </li>
-      <li className={s.item}>
-        <span className={s.label}>{label}</span>
-        <span className={s.percentage}>{percentage}%</span>
-      </li>
-      <li className={s.item}>
-        <span className={s.label}>{label}</span>
-        <span className={s.percentage}>{percentage}%</span>
+        {labels.map(label => (
+          <span className={s.label}>{label}</span>
+        ))}
+        ;
+        {percentages.map(percentage => (
+          <span className={s.percentage}>{percentage}%</span>
+        ))}
       </li>
     </ul>
   );
 }
 
 Statistics.propTypes = {
-  label: PropTypes.string,
-  percentage: PropTypes.number,
+  labels: PropTypes.string,
+  percentages: PropTypes.number,
 };
